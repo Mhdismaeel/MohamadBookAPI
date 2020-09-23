d17bdd7a19a9e2c510f0465aaca169c823f4b5ed
Mhdismaeel <34945797+Mhdismaeel@users.noreply.github.com>
2020-09-23 15:43:22 +0300

    Update Order.php

--- app/Order.php
+++ app/Order.php
@@ -9,7 +9,7 @@ class Order extends Model
 {
     public function orderbook()
     {
-        return $this->belongsToMany(Book::class);
+        //return $this->belongsToMany(Book::class);
     }
 
 }
